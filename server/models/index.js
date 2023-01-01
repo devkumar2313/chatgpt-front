dd05b51670e966f31cf5dd3dab7f7c91d863ae87
dev kumar <devkumarpal333@gmail.com>
2023-01-01 11:04:10 +0530

    initial commit

--- server/models/index.js
+++ server/models/index.js
@@ -9,13 +9,6 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-const Sequelize = new Sequelize("Education", "root", "", {
-  host: "127.0.0.1",
-  dialect: "mysql",
-  dialectOptions: {
-    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  },
-});
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
