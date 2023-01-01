803996ddb69e1929cec53d610d73806786e6f677
dev kumar <devkumarpal333@gmail.com>
2023-01-01 17:51:52 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 3303;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(cors());
